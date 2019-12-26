7751cf9d0fecadfd8e718153ac4f1d03895f724e
BarbuDev <bastien.canivez@outlook.fr>
2019-12-26 16:04:18 +0100

    Clean classes for new system factory

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,39 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.util.Map;
+
+/**
+ * Start the application
+ * Write a file result with sorted and counted elements from an other file
+ *
+ * @see ISymptomeWriter
+ * @see ISymptomReader
+ * @see SymptomDataWriterFactory
+ * @see SymptomDataFromFileFactory
+ *
+ * @author BarbuDev
+ * @version 1.0
+ *
+ */
+
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
+
 	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
 
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+		Map<String,String> symptomMap;
+		// Start factory for reader and writer
+		SymptomDataFromFileFactory symptomReader = new SymptomDataFromFileFactory();
+		SymptomDataWriterFactory symptomWriter = new SymptomDataWriterFactory();
+
+		// Instanciation of reader and writer with the factories
+		ISymptomReader dataReader = symptomReader.getReader("TXT") ;
+		ISymptomeWriter dataWriter = symptomWriter.getWriter("OUT");
+
+		// Write result
+		symptomMap = dataReader.getSymptoms("symptoms");
+		dataWriter.writeSymptoms(symptomMap, "result");
+
 	}
 }
